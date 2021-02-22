bf7f54845cd78010fd058e0c967894d116442421
Javad <abdijavad110@gmail.com>
2021-02-22 18:03:42 +0330

    fix: fix last commit

--- common/core/memory_subsystem/cache/chunk_manager.cc
+++ common/core/memory_subsystem/cache/chunk_manager.cc
@@ -2,7 +2,7 @@
 #include "simulator.h"
 
 
-float qual_array[] = {10e-3, 10e-4, 10e-5, 10e-6, 10e-7};
+float qual_array[] = {1e-3, 1e-4, 1e-5, 1e-6, 1e-7};
 int qual_arr_size = sizeof(qual_array)/sizeof(qual_array[0]);
 
 void ChunkManager::new_acc(int addr) {
@@ -14,12 +14,16 @@ void ChunkManager::update_table() {    // fixme: (JH) use properly
     float spatial = dens.spatial;
     float temporal = dens.temporal;
 
+    printf("\n\nInside update========================================================\n");
     for(int i = 0; i < approx_table_max_entry; i++) {
 //        double new_spat_qual = get_qual(Sim()->approx_table[i].spat.min, Sim()->approx_table[i].spat.max, spatial);
         double new_temp_qual = get_qual(Sim()->approx_table[i].temp.min, Sim()->approx_table[i].temp.max, temporal);
         //fixme: felan faghat temporal
+        printf("%.9f -> ", Sim()->approx_table[i].quality_level);
         Sim()->approx_table[i].quality_level = new_temp_qual;
+        printf("%.9f\t", Sim()->approx_table[i].quality_level);
     }
+    printf("========================================================\n");
     current_chunk.reset();
 
 //        if((address >= (IntPtr) approx_table[i].start_address) && (address <= (IntPtr) approx_table[i].end_address))
@@ -28,7 +32,7 @@ void ChunkManager::update_table() {    // fixme: (JH) use properly
 
 double
 ChunkManager::get_qual(float min, float max, float current){
-//    return qual_array[qual_arr_size-1];
+//    return 1e-5;
     if (current == 0) return 0;
     if (current <= min)
         return qual_array[0];
